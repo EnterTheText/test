849fb77aa7e6524e38fb10c5fac996805a852f5b
GP <jenyasm88@gmail.com>
2025-12-09 09:31:27 +0300

    09.12.1

--- wow.py
+++ wow.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 class Alien:
     al_count = 0
@@ -11,7 +12,22 @@ class Alien:
         self.p -= d
     def is_alive(self):
         return self.p > 0
+
+def u(_x, _y):
+    global xy
+    px, py = random.uniform(0, 100), random.uniform(0, 100)
+    flag = True
+    while flag:
+        if (px, py) not in xy:
+            flag = False
+            xy.add((px, py))
+        else:
+            px, py = random.uniform(0, 100), random.uniform(0, 100)
+    return aliens.
 aliens = []
+x = []
+y = []
+xy = set()
 for i in range(100):
     aliens.append(Alien(random.randint(0, 100), random.randint(0, 100), random.randint(1, 100)))
 print(aliens)
