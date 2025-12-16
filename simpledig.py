fc117dfb7a9310e09301282d0103cbf3a3e908dc
GP <jenyasm88@gmail.com>
2025-12-16 09:28:12 +0300

    16.12.1

---
+++ simpledig.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+def func(p):
+    m = 0
+    while p != 1:
+        if p % 2 == 0: p /= 2
+        else: p = p * 3 + 1
+        m += 1
+    return m
+
+
+k = int(input())
+c = 0
+x = []
+y = []
+for n in range(3, k+1):
+    for i in range(2, n+1):
+        if n % i == 0: c += 1
+    if c == 1:
+        x.append(n)
+        y.append(func(n))
+    c = 0
+print("x:", *x)
+print("y:", *y)
+
+plt.plot(x, y, )
+
+x = []
+y = []
+for l in range(1, k+1):
+    x.append(l)
+    y.append(func(l))
+
+print("\nx:", *x)
+print("y:", *y)
+plt.plot(x, y, color="red")
+plt.show()
